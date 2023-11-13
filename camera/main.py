d71b5084b6daa004d3b209b0c8cfaba67bb89b30
Matias Andina <matiasandina@gmail.com>
2023-11-13 11:01:17 -0500

    adding some parameter input

--- camera/main.py
+++ camera/main.py
@@ -3,15 +3,16 @@ import cv2
 import datetime
 
 def main():
-    vc = VideoCamera(usePiCamera=False, record=True, record_duration="00:00:10")
+    record_duration = "00:05:00" # change duration of recording here
+    record_name = None # change from None to something else for naming
+    vc = VideoCamera(usePiCamera=False, record=True, record_duration=record_duration, record_name = record_name)
 
     print("Starting camera preview. Press 'r' to start/stop recording, 'q' to quit.")
     recording = False
     recording_start_time = None
     while True:
         frame = vc.read()
-        cv2.imshow("Camera Preview", frame)
-
+        
         if recording:
             if not vc.is_recording():
                 recording = False
@@ -19,8 +20,10 @@ def main():
             else:
                 elapsed_time = datetime.datetime.now() - recording_start_time
                 elapsed_str = str(elapsed_time).split('.')[0]  # Format as HH:MM:SS
-                print(f"Status: Recording {elapsed_str} / 00:00:10", end='\r')
-
+                print(f"Status: Recording {elapsed_str} / {record_duration}", end='\r')
+        else:
+            cv2.putText(frame, "NOT RECORDING", (frame.shape[0] // 5 , frame.shape[1] // 3), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+        cv2.imshow("Camera Preview", frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('r'):
             if not recording:
@@ -33,6 +36,10 @@ def main():
                 recording = False
                 print("\nRecording stopped.")
         elif key == ord('q'):
+            if recording:
+                vc.stop_recording()
+                recording = False
+                print("\nRecording stopped.")
             break
 
     # Cleanup
