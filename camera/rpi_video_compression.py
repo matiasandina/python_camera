d379e0392e868f547d788c511fe918f510837728
skrigel <skrigel@mit.edu>
2024-06-19 12:04:51 -0400

    Fixing messed up file path checking

--- camera/rpi_video_compression.py
+++ camera/rpi_video_compression.py
@@ -21,18 +21,24 @@ def compress_videos(animal_id, crf, base_folder, out_folder):
             session_id = match.group(1).replace('-', '').replace('T', '').replace(':', '')
             base_name = os.path.basename(file)
             file_name, file_extension = os.path.splitext(base_name)
-            output_file = os.path.join(out_folder, session_id, "beh", f'sub-{animal_id}_ses-{session_id}{file_extension}')
+
+            out_folder = os.path.join(out_folder, session_id, "beh")
+
+            if not os.path.exists(out_folder):
+                os.makedirs(out_folder)
+
+            output_file = os.path.join(out_folder, f'sub-{animal_id}_ses-{session_id}{file_extension}')
 
             # Process .mp4 files
             if file_extension == '.mp4':
-                output_file = os.path.join(out_folder, session_id, "beh", f'sub-{animal_id}_ses-{session_id}_video{file_extension}')
+                output_file = os.path.join(out_folder, f'sub-{animal_id}_ses-{session_id}_video{file_extension}')
                 cmd = f"ffmpeg -i {os.path.join(base_folder, file)} -c:v libx264 -crf {str(crf)} -c:a copy {output_file}"
                 subprocess.run(cmd, shell=True, check=True)
                 print(f"Video file compressed with crf {crf} and copied to folder {out_folder}")
 
             # Process .csv files
             elif file_extension == '.csv':
-                output_file = os.path.join(out_folder, session_id, "beh", f'sub-{animal_id}_ses-{session_id}_timestamps{file_extension}')
+                output_file = os.path.join(out_folder, f'sub-{animal_id}_ses-{session_id}_timestamps{file_extension}')
                 shutil.copy(os.path.join(base_folder, file), output_file)
                 print(f"Timestamps file copied to folder {out_folder}")
 
