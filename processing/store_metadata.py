8753f68b66dbe64d99d5a7f3f4ca4d2bb8e9bcb6
wonicorn <sasha.krigel@gmail.com>
2024-06-28 10:12:41 -0400

    fixing ffmpeg cropping

--- processing/store_metadata.py
+++ processing/store_metadata.py
@@ -18,9 +18,7 @@ def get_session(video_path, type = "str"):
         # Create a new datetime object
         timestamp_dt = datetime(timestamp_dt.year, timestamp_dt.month, timestamp_dt.day)
 
-        timestamp_str = datetime.strftime(timestamp_dt, "%YYYY%mm%dd")
-
-
+        timestamp_str = datetime.strftime(timestamp_dt, "%Y%m%d")
         if type == "str":
             return timestamp_str
         if type == "dt":
@@ -31,8 +29,6 @@ def get_session(video_path, type = "str"):
 
 def get_session_type(exp_dates, target_date):
 
-    print(target_date, "target")
-    print(exp_dates, "exp_dates")
     date_to_exp_type = {date: key for key, date_list in exp_dates.items() for date in date_list}
     return date_to_exp_type[target_date]
 
@@ -45,29 +41,49 @@ def get_cropping_coords(filepath):
     regions['frame_shape'] = selector.get_shape()
     selector.release()
 
-    return selector.regions
 
-def get_session_metadata(animal_dir, exp_dates):
+    return regions
 
+def get_session_metadata(animal_dir, exp_dates, coords_by_session):
     files = os.listdir(animal_dir)
     session_metadata = {}
     for file in files:
-        filepath = os.path.join(animal_dir, file)
-        session_metadata['filepath'] = filepath
-        session_datetime = get_session(file, type = "dt")
-
-
-        session_metadata['date'] = session_datetime
-
-        #expects exp_dates to be datetime objects
-        session_metadata["session_type"] = get_session_type(exp_dates, session_datetime)
-        session_metadata["coords"] = get_cropping_coords(filepath)
+        if file.endswith(".mp4"):
+            session = {}
+            name = get_session(file, type = "str")
+            filepath = os.path.join(animal_dir, file)
+            session['filepath'] = filepath
+            session_datetime = get_session(file, type = "dt")
+            session['date'] = session_datetime
+            #expects exp_dates to be datetime objects
+            session_type = get_session_type(exp_dates, session_datetime)
+            session["session_type"] = session_type
+            if coords_by_session == None:
+                coords = get_cropping_coords(filepath)
+            session["coords"] = coords_by_session[session_type]
+            session_metadata[name] = session
 
     return session_metadata
 
+def crop_coords_by_session(exp_dates, animal_dir):
+    videos = [video for video in os.listdir(animal_dir) if video.endswith(".mp4")]
+    # print(get_session(video, type = "dt"))
+    fasted = [video for video in videos if get_session_type(exp_dates,  get_session(video, type = "dt")) == "fasted"]
+    baseline = [video for video in videos if get_session_type(exp_dates,  get_session(video, type = "dt")) == "baseline"]
 
-def populate_dictionary(animal_dir, animal_id, exp_dates, dob = [], sex = [], mac = [], fed = [], session_metadata = {}):
+    if fasted:
+        fasted_coords = get_cropping_coords(os.path.join(animal_dir, fasted[0]))
+    else:
+        fasted_coords = None
 
+    if baseline:
+        baseline_coords = get_cropping_coords(os.path.join(animal_dir, baseline[0]))
+    else:
+        baseline = None
+
+    return {"fasted": fasted_coords, "baseline": baseline_coords}
+
+def populate_dictionary(animal_dir, animal_id, exp_dates, dob = [], sex = [], mac = [], fed = [], session_metadata = {}, coords_by_session = None):
     d = {}
     d["animal_id"] = animal_id
     #must be datetime object
@@ -76,8 +92,8 @@ def populate_dictionary(animal_dir, animal_id, exp_dates, dob = [], sex = [], ma
     d["sex"] = sex
     d["mac"] = mac
     d["fed"] = fed
-
-    session_metadata = get_session_metadata(animal_dir, exp_dates)
+    if session_metadata == {}:
+        session_metadata = get_session_metadata(animal_dir, exp_dates, coords_by_session)
     d['session_metadata'] = session_metadata
     return d
 
@@ -94,7 +110,6 @@ def flatten_dict(data):
     return flattened_data
 
 def write_parquet(animal_dir, data):
-
     animal_id = data["animal_id"]
     flattened_data = flatten_dict(data)
     df = pd.DataFrame([flattened_data])
@@ -111,10 +126,9 @@ def dict_to_parquet(data, parquet_path):
 
 def read_metadata(parquet_path):
     df = pd.read_parquet(parquet_path)
-
+    # TODO: this will get you the first row only.
+    # We shouldn't have more than one, but it's not asserted
     return df.loc[0, :].to_dict()
-
-
 
 
 if __name__ == "__main__":
@@ -124,9 +138,51 @@ if __name__ == "__main__":
     exp_dates =  {"fasted": ["20240620", "20240621"], "baseline": ["20240617", "20240618"]}
     #convert dates to datetime
     exp_dates_dt = {k: [datetime.strptime(val, "%Y%m%d") for val in v] for k, v in exp_dates.items()}
+    animal_list = ["MLA163"]
 
-    # val_dict = populate_dictionary(animal_dir, "MLA163", exp_dates_dt)
-    # write_parquet(animal_dir, val_dict)
 
-    md_dict = read_metadata("processing/animal_dir/MLA163_metadata.parquet")
-    print(md_dict)
+    for animal_id in animal_list:
+        # animal_dir = os.path.join(video_dir, animal_id)
+
+        coords_by_session = crop_coords_by_session(exp_dates_dt, animal_dir)
+        metadata = populate_dictionary(animal_dir, animal_id, exp_dates_dt, coords_by_session = coords_by_session)
+        write_parquet(animal_dir, metadata)
+
+        for file in os.listdir(animal_dir):
+
+            if file.endswith(".mp4"):
+                name = get_session(file, type = "str")
+                coords = metadata["session_metadata"][name]["coords"]
+                light_cage_coords = coords["light_cage"]
+                dark_cage_coords =coords["dark_cage"]
+                # Sample coordinates
+                # light_cage_coords = [(110, 286), (200, 123), (463, 329)]
+                # dark_cage_coords = [(397, 80), (396, 325), (497, 329), (501, 83)]
+                y_px_tolerance = 10
+                # Calculate encompassing Y-boundaries with a tolerance of 10 pixels
+                min_y = min(coord[1] for coord in light_cage_coords + dark_cage_coords) - y_px_tolerance
+                max_y = max(coord[1] for coord in light_cage_coords + dark_cage_coords) + y_px_tolerance
+                range_y = max_y - min_y
+                # this is data we already have but to be consistent with naming
+                min_x = 0 # this is hardcoded though
+                range_x = coords["frame_shape"][0]
+                # maybe some assertions here to know we are not trying to get out of the frame
+
+                print(range_x, range_y, min_x, min_y)
+
+                input_video_path = os.path.join(animal_dir, file)
+
+                root, ext = os.path.splitext(file)
+                    # but maybe also some overengineering
+                output_video_path = os.path.join(animal_dir,f"{root}_cropped.mp4")
+
+                cmd_command=command = [
+                        "ffmpeg",
+                        "-i", input_video_path,
+                        "-vf", f"crop={range_x}:{range_y}:{min_x}:{min_y}",
+                        "-c:v", "libx264",
+                        "-crf", "18",
+                        "-c:a", "copy",
+                        output_video_path]
+
+                subprocess.run(cmd_command)
